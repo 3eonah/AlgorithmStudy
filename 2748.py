41539ee7cc4ee2b73e2ef3be5ca7c946d28f2ee5
3eonah <3eonah.lee@gmail.com>
2023-04-06 20:59:43 +0900

    Final Commit:리스트 이용

--- 2748.py
+++ 2748.py
@@ -1,12 +1,28 @@
-def Fibo(n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    elif n>=2:
-        return Fibo(n-1)+Fibo(n-2)
+# 재귀함수 이용 -> 시간초과
+# def Fibo(n):
+#     if n==0:
+#         return 0
+#     elif n==1:
+#         return 1
+#     elif n>=2:
+#         return Fibo(n-1)+Fibo(n-2)
 
-N=int(input())
+# N=int(input())
 
-if 0<=N<=90:
-    print (Fibo(N))
+# if 0<=N<=90:
+#     print (Fibo(N))
+#--------------------------
+
+# 리스트 이용
+n=int(input())
+
+if 0<=n<=90:
+    Fibo=[]
+    for i in range(n+1):
+        if i==0:
+            Fibo.append(0)
+        elif i==1:
+            Fibo.append(1)
+        else:
+            Fibo.append(Fibo[i-2]+Fibo[i-1])
+    print(Fibo[n])
