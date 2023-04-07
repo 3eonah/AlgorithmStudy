f361c595706605538df2d5eca03751fba81ae5d3
3eonah <3eonah.lee@gmail.com>
2023-04-07 14:43:55 +0900

    Additonal Commit:메모화, 조기리턴 활용

--- 2748.py
+++ 2748.py
@@ -1,28 +1,11 @@
-# 재귀함수 이용 -> 시간초과
-# def Fibo(n):
-#     if n==0:
-#         return 0
-#     elif n==1:
-#         return 1
-#     elif n>=2:
-#         return Fibo(n-1)+Fibo(n-2)
+# 재귀함수 메모리제이션과 조기리턴 활용
+Memo={0:0, 1:1}
+def Fibo(n):
+    if n in Memo:   # 조기리턴
+        return Memo[n]
+    output = Fibo(n-1)+Fibo(n-2)
+    Memo[n]=output
+    return output
 
-# N=int(input())
-
-# if 0<=N<=90:
-#     print (Fibo(N))
-#--------------------------
-
-# 리스트 이용
-n=int(input())
-
-if 0<=n<=90:
-    Fibo=[]
-    for i in range(n+1):
-        if i==0:
-            Fibo.append(0)
-        elif i==1:
-            Fibo.append(1)
-        else:
-            Fibo.append(Fibo[i-2]+Fibo[i-1])
-    print(Fibo[n])
+N=int(input())
+print(Fibo(N))
