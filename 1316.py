ccd8802b223d4397105466221a4031e68b912dcf
3eonah <3eonah.lee@gmail.com>
2023-04-10 20:16:47 +0900

    First Commit:단어개수만큼단어입력받기

---
+++ 1316.py
@@ -0,0 +1,18 @@
+# 그룹단어 체커
+# 각 문자가 연속해서 나타나는 그룹단어의 개수 출력
+# ccazzzzbb -> 그룹단어
+# aabbba -> 그룹단어 x
+
+
+N=int(input())
+words=[]
+for i in range(N):
+    str=input()
+    words.append(str)
+
+for i in range(len(words)):
+    
+
+    
+
+
