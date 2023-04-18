5dd1d89af6bf3f2b0b817bb1423854b55bceb71b
3eonah <3eonah.lee@gmail.com>
2023-04-18 16:21:48 +0900

    Commit Beakjoon No.1316

--- 1316.py
+++ 1316.py
@@ -5,12 +5,30 @@
 
 
 N=int(input())
-words=[]
+count=N     # 그룹단어의 개수 count
+
 for i in range(N):
     str=input()
-    words.append(str)
+    breaker=False       # 2중 for문을 멈추기 위한 breaker
 
-for i in range(len(words)):
+    for j in range(len(str)-1):
+        if breaker==True:
+            break
+
+        elif str[j]==str[j+1]:
+            continue
+        
+        else:
+            for k in range(j+2, len(str)):
+                if str[j]==str[k]:
+                    count-=1
+        
+                    breaker=True
+                    break
+
+print(count)
+
+
     
 
     
